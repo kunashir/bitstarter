2d124a5b885889867642b868446810ac87048322
Korolev A N <kunashir@list.ru>
2013-09-12 14:02:12 +0000

    Change webjs to read index.html

--- web.js
+++ web.js
@@ -3,8 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+  buff =fs.readFileSync("index.html");
+  response.send(buff.toString());
+}
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
